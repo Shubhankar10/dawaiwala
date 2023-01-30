11bf3152b2ac078246f04b644c21fd03766f382a
Krish Chopra <krishchopra22@gmail.com>
2023-01-30 16:18:03 +0530

    updated homeScreen

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -6,7 +6,56 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(16),
       color: Colors.blueAccent,
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: SafeArea(
+          child: Column(
+        children: [
+          Container(
+            alignment: Alignment.bottomCenter,
+            padding: const EdgeInsets.all(10),
+            height: 200,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: Color(0xff1870B5),
+            ),
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    //Navigator.of(context).pop();
+                  },
+                  child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        margin: const EdgeInsets.only(bottom: 80),
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            border: Border.all(width: 2, color: Colors.white)),
+                        child: const Icon(
+                          Icons.account_circle,
+                          color: Colors.white,
+                        ),
+                      )),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(80.0),
+                    ),
+                    suffixIcon: const Icon(Icons.search),
+                    filled: true,
+                    fillColor: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      )),
     );
   }
 }
